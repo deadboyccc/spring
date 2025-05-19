8f1ffe767626e2dcf73982deef7f697a5a294bd2
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2025-05-19 15:24:56 +0300

    the new main project directory

---
+++ vstest/src/main/java/com/example/vstest/VstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.vstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VstestApplication.class, args);
+	}
+
+}
---
+++ vstest/src/test/java/com/example/vstest/VstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
